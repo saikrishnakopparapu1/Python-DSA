a89ab9bd4ba2a6e66ef8dd01affb9b6150704b9e
saikrishnakopparapu1 <saikrishna.kopparapu1@gmail.com>
2026-03-08 18:16:59 +0530

    this is variable.py

---
+++ variables.py
@@ -0,0 +1,10 @@
+print("hello world")
+age = 25
+name = input("Enter your name: ")   
+company = input("Enter your company name: ")
+#company = "Tata Consultancy services"
+Location = input("Enter your location: ")
+_country = input("Enter your country: ")
+#Location = "Hyderabad" 
+#_country = "India"
+print("My name is", name, "and I am", age, "years old. I work at", company, "in", Location, ",", _country)
